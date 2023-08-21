f79ffa035073441e8cf3ff6dd27e1323d1c08c07
Santicm23 <santicastrom@outlook.com>
2023-08-21 10:52:08 -0500

    creating card layout

--- app/src/main/java/com/example/taller1/CountriesActivity.kt
+++ app/src/main/java/com/example/taller1/CountriesActivity.kt
@@ -2,10 +2,16 @@ package com.example.taller1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.example.taller1.databinding.ActivityCountriesBinding
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
 
 class CountriesActivity : AppCompatActivity() {
+
+    companion object {
+        private const val COUNTRIES_FILE = "paises.json"
+    }
 
     private lateinit var binding: ActivityCountriesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,10 +21,31 @@ class CountriesActivity : AppCompatActivity() {
 
         val regionSelected = intent.getStringExtra("region")
 
-        if (regionSelected is String) {
-            Log.d("region", regionSelected)
-        } else {
+        if (regionSelected !is String)
             throw Error("No region received")
+
+        binding.countriesTitle.text = "Countries of region: ${regionSelected}"
+
+    }
+
+    fun loadJSONFromAsset(assetName: String): String {
+        val json: String
+        json = try {
+            val inputStream = this.assets.open(assetName)
+            val size = inputStream.available()
+            val buffer = ByteArray(size)
+            inputStream.read(buffer)
+            inputStream.close()
+            String(buffer, charset("UTF-8"))
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+            ""
         }
+        return json
+    }
+
+    @Throws(JSONException::class)
+    fun loadCountriesByJson(): JSONObject {
+        return JSONObject(loadJSONFromAsset(COUNTRIES_FILE))
     }
 }
