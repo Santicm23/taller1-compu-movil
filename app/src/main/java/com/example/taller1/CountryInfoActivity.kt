61f595d51c10eefa60ee8b35b6dd716dd02edec8
Santicm23 <santicastrom@outlook.com>
2023-08-23 21:59:00 -0500

    finish :)

--- app/src/main/java/com/example/taller1/CountryInfoActivity.kt
+++ app/src/main/java/com/example/taller1/CountryInfoActivity.kt
@@ -3,17 +3,32 @@ package com.example.taller1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.bumptech.glide.Glide
+import com.example.taller1.databinding.ActivityCountryInfoBinding
+import org.json.JSONObject
 
 class CountryInfoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCountryInfoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_country_info)
+
+        binding = ActivityCountryInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val countrySelected = intent.getStringExtra("country")
 
         if (countrySelected !is String)
             throw Error("No region received")
 
-        Log.d("country", countrySelected)
+        val infoCountry = JSONObject(countrySelected)
+
+        binding.countryName.text = "${infoCountry.getString("Name")} - ${infoCountry.getString("NativeName")}"
+        binding.alphaCodes.text = "${infoCountry.getString("Alpha2Code")} - ${infoCountry.getString("Alpha3Code")}"
+        binding.language.text = infoCountry.getString("NativeLanguage")
+        binding.regions.text = "${infoCountry.getString("Region")}/${infoCountry.getString("SubRegion")}"
+        binding.currencyInfo.text = "${infoCountry.getString("CurrencyName")} (${infoCountry.getString("CurrencySymbol")})"
+        Glide.with(binding.imageView.context).load(infoCountry.getString("FlagPng")).into(binding.imageView)
     }
 }
